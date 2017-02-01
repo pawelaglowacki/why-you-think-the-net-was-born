af74a35879972bc34fde55b2e77dd2c58da94615
pawelaglowacki <pawelglowackicapgemini@gmail.com>
2017-02-02 01:08:25 +0300

    only correct urls

--- cycki.py
+++ cycki.py
@@ -5,9 +5,12 @@ import random
 
 zeros = "00000"
 
-for i in range(1,99999):
+for i in range(0,99999):
   try:
     boob_id = zeros[0:len(zeros)-len(str(i))] + str(i)
-    urllib.urlretrieve("http://media.oboobs.ru/boobs/" + boob_id + ".jpg", boob_id + ".jpg")
+
+    a=urllib.urlopen("http://media.oboobs.ru/boobs/" + boob_id + ".jpg")
+    if a.getcode() == 200:
+    	urllib.urlretrieve("http://media.oboobs.ru/boobs/" + boob_id + ".jpg", boob_id + ".jpg")
   except:
     pass
